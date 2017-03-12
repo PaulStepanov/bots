1efde41353fa644050050d6c1471ae88720c8877
Павел <freestalker1987@gmail.com>
2017-03-12 16:47:21 +0300

    readyToDeply

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let builder = require('botbuilder');
 let restify=require('restify');
 
@@ -9,12 +8,16 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 
 // Create chat bot
-let connector = new builder.ConsoleConnector().listen();
-let bot = new builder.UniversalBot(connector);
+var connector = new builder.ChatConnector({
+    appId: 'c9d75f98-7c60-47b7-b7e0-52a785819aeb',
+    appPassword: 'Pfjhd6RnP7A00OzN0W2QeVB'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
 
 //Configure bot intends and dialogs
-let intents=require('./saveIntends');
+let intents=require('./src/saveIntends');
 bot.dialog('/', intents);
 
-let dialog=require('./dialogs');
+let dialog=require('./src/dialogs');
 console.log(dialog.call(this,bot));
