1984d12e4caca430c11c24dc8690a5d55f37d6a9
zarabon <freestalker1987@gmail.com>
2017-03-14 09:23:22 +0300

    added env differences

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 let builder = require('botbuilder');
-let restify=require('restify');
+let restify = require('restify');
 
-//Setup server
-let server=restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
-});
-
-// Create chat bot
 let connector;
-connector = new builder.ChatConnector({
-    appId: 'c9d75f98-7c60-47b7-b7e0-52a785819aeb',
-    appPassword: 'Pfjhd6RnP7A00OzN0W2QeVB'
-});
-// connector = new builder.ConsoleConnector().listen();
-let bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
+if (process.env.NODE_ENV == 'production') {
+    //Setup server
+    let server = restify.createServer();
+    server.listen(process.env.port || process.env.PORT || 3978, function () {
+        console.log('%s listening to %s', server.name, server.url);
+    });
+    // Create chat bot
+    connector = new builder.ChatConnector({
+        appId: process.env.MICROSOFT_APP_ID,
+        appPassword: process.env.MICROSOFT_APP_PASSWORD
+    });
+    server.post('/api/messages', connector.listen());
+} else {
+    console.log('started console connector')
+    //If not production use console connector
+    let connector = new builder.ConsoleConnector().listen()
+}
 
-//Configure bot intends and dialogs
-let intents=require('./src/mainIntends');
+let bot = new builder.UniversalBot(connector);
+
+//Bootstrap app
+let intents = require('./src/mainIntends');
 bot.dialog('/', intents);
 
-let dialog=require('./src/dialogs');
-console.log(dialog.call(this,bot));
+let dialog = require('./src/dialogs');
+dialog.call(this, bot);
